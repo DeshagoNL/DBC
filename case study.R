c7a1736dc8617ce9448f6f7b3c1c9da187ff69bb
DeshagoNL <drakennl@gmail.com>
2021-09-13 09:28:00 +0200

    added case study

---
+++ case study.R
@@ -0,0 +1,13 @@
+# hy is data driven decision making useful in this business case: 
+# By using data, both from the ERP system and the machines that are used in production,
+# more insight can be gathered into the flow of production.
+# This way bottlenecks and problems can be solved and the companies can better
+# utilize their machines
+
+# How will the organization apply 3DM?
+# They will use 3DM to combine the data from multiple sources into one clear picture,
+# probably using a dashboard, which can be used to improve the flow of production
+
+# What can the organization do with 3DM that they couldn't do before?
+# they can combine the data from multiple sources and combine them. this way they can
+# see where the bottlenecks are and how the products flow between the machines.
